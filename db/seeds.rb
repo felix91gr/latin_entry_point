6eb7f82f3d3a62360169f48df287b10532eb4e1a
felix91gr <felix91gr@gmail.com>
2017-07-08 00:39:06 -0400

    Added first seeds. TODO: complete Abbr seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+### Gender seeds #######################
+
+g_masc = Gender.create(name: "masculino")
+g_feme = Gender.create(name: "femenino")
+g_neut = Gender.create(name: "neutro")
+
+### Singular Genitive seeds ############
+#TODO: Change this model to "Declination" instead of "SingGenitive"??
+
+gs_ae = SingularGenitive.create(genitive: "ae, es")
+gs_i  = SingularGenitive.create(genitive: "i" )
+gs_is = SingularGenitive.create(genitive: "is")
+gs_us = SingularGenitive.create(genitive: "us")
+gs_ei = SingularGenitive.create(genitive: "ei")
+
+### Abbreviature seeds #################
+
+abbr_abl = Abbreviature.create(explanation: "ablativo")
+Abbreviation.create(name: "abl", abbreviature: abbr_abl)
